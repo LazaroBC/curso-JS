8f4a43894bfc01f9a06771b5ff293352ae206695
Your Name <you@example.com>
2020-07-06 18:55:27 +0200

    clases y subclases

---
+++ 01-fundamentos/assets/js/classes/sub-classes.js
@@ -0,0 +1,62 @@
+class Persona {
+
+
+    static _conteo = 0;
+    static get conteo(){
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje(){
+        console.log(this.nombre);// undefined
+        console.log('Hola, soy un metodo estático');
+    }
+
+    nombre = '';
+    codigo = '';
+    frase  = '';
+    comida = '';
+    
+    constructor(
+        nombre = 'Sin nombre',
+        codigo = 'Sin código',
+        frase  = 'Sin frase',
+    ) {
+
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase  = frase;
+        Persona._conteo++;
+    }
+
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita( ) {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`
+    }
+
+    quienSoy(){
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase(){
+        console.log(`${this.nombre} - ${this.frase}`);
+    }
+}
+
+class Heroe extends Persona{
+    
+    clan = 'sin clan';
+    constructor ( nombre, codigo, frase ){
+        super(nombre, codigo, frase);
+        this.clan = 'Avengers'
+    }
+    quienSoy(){
+        console.log(`Soy ${this.nombre}, ${this.clan}`);
+        super.quienSoy();
+    };
+}
+const spiderman = new Heroe('Peter Parker', 'Spiderman', 'Soy tu amigo Spiderman');
+console.log(spiderman);
+spiderman.quienSoy();
